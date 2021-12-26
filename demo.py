55d0d700ca146b6927cf027ca49af6cabb056f8e
desyatkoveg <93652524+desyatkoveg@users.noreply.github.com>
2021-12-26 21:22:55 +0700

    start_screen + broken menu

--- demo.py
+++ demo.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import numpy as np
 import pygame as pg
 from pygame import time
 
@@ -17,51 +16,37 @@ def load_image(name):
 
 
 def start_screen():
-    # Выводим изображение заставки:
+    """Стартовое окно"""
     start_screen_background = load_image('fon1.jpeg')
     screen.blit(start_screen_background, (0, 0))
-    # Выводим текст заставки:
-    font = pg.font.Font(None, 30)
-    text_coord = 10
-    # Главный игровой цикл для окна заставки:
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 terminate()
             elif event.type == pg.KEYDOWN or event.type == pg.MOUSEBUTTONDOWN:
-                return  # Начинаем игру
+                return
         pg.display.flip()
-
 
 
 def terminate():
     """Выход из игры"""
-    # Определяем отдельную функцию выхода из игры,
-    # чтобы ею можно было воспользоваться,
-    # как при закрытии игрового окна,
-    # так и при закрытии окна заставки:
     pg.quit()
     sys.exit()
-
-
-class Menu():
-    pass
 
 
 if __name__ == '__main__':
     pg.init()
     pg.display.set_caption('Wave of Zombies')
-    pg.display.set_icon(pg.image.load('/home/nastya/code/waveOfZombies/data/mozg.png'))
+    icon = load_image('mozg.png')
+    pg.display.set_icon(icon)
     size = width, height = 1280, 720
     screen = pg.display.set_mode(size)
 
-    # Выводим заставку игры:
     start_screen()
 
-    # Задаём задержку автоповторения нажатой клавиши:
     pg.key.set_repeat(200, 70)
 
-    # Главный игровой цикл:
     fps = 60
     running = True
     while running:
@@ -69,6 +54,8 @@ if __name__ == '__main__':
             if event.type == pg.QUIT:
                 running = False
         screen.fill(pg.Color('black'))
+        menu = load_image('menu.png')
+        screen.blit(menu, (0, 0))
         pg.display.flip()
         time.Clock().tick(fps)
     terminate()
